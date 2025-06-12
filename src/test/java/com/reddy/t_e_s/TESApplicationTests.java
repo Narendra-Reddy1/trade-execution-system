a5aeb39fe768da8dfd64a6be47115954de9ec48c
Narendra-Reddy <narendrareddy54321@gmail.com>
2025-06-12 17:00:29 +0530

    Initial Changes

---
+++ src/test/java/com/reddy/t_e_s/TESApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reddy.t_e_s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TESApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
